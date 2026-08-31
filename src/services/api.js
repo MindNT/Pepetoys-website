7945075db0f6bbbb4916dfc429609481ba218157
Adrian Carmona <adrianrobertocarmona49@gmail.com>
2026-08-30 23:34:45 -0600

    New version with new route

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'https://pepetoys-backend.mindnt.com.mx';
+export const API_BASE_URL = 'https://pepetoys-backend.mindnt.com.mx/api/';
 //export const API_BASE_URL = 'http://localhost:5010';
 // export const PAYMENT_API_BASE_URL = 'http://128.199.0.156:9456';
 export const PAYMENT_API_BASE_URL = 'https://pepetoys-backend.mindnt.com.mx';
